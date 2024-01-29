1655dfa580d7242982db8bcd4d85215032fe9b02
Maham Aziz <maham1aziz2@gmail.com>
2024-01-29 00:45:34 -0700

    Fixing Biggest Chunk of Errors
    Program is now 90% complete

--- Project1 - bagels.py
+++ Project1 - bagels.py
@@ -20,9 +20,10 @@ print(numb)
 print()
 
 guess_count = 0
-sim_count = 0
-aa = 0
 while guess_count <= 10:
+    final = []
+    n_list = []
+    g_list = []
     guess = input("Please enter you number guess: ")
     if guess == numb:
         print("You got it!")
@@ -33,38 +34,35 @@ while guess_count <= 10:
             exit()
     else:
         for x in numb:
-            print(x)
-            if x in guess:
-                aa += 1
-                print(aa + 101)
-                if aa == 1:
-                    index1 = numb.index(x)
-                    index2 = guess.index(x)
-                    if index1 == index2:
-                        print("Fermi")
-                    else:
-                        print("Pico")
-                else:
-                    print("Bagels")
+            n_list.append(x)
+        for y in guess:
+            g_list.append(y)
+        if n_list[0] in g_list:
+            if n_list[0] == g_list[0]:
+                final.append("Fermi")
+                print(final)
             else:
-                print("Bagels")
-    guess_count += 1    
-                
-            #else:
-                #print("")
-            #str_guess = str(guess)
-            #find = str_guess.count(x)
-            #if find == 1:
-            #    str(numb)
-            #    find_numb = numb.find(x)
-            #    find_guess = guess.find(x)
-            #    if find_numb == find_guess:
-            #        print("Fermi")
-            #    else:
-            #        print("Pico")
-            #elif find == 0:
-            #    print("Bagels")
-   # guess_count += 1
+                final.append("Pico")
+                print(final)
+        if n_list[1] in g_list:
+            if n_list[1] == g_list[1]:
+                final.append("Fermi")
+                print(final)
+            else:
+                final.append("Pico")
+                print(final)
+        if n_list[2] in g_list:
+            if n_list[2] == g_list[2]:
+                final.append("Fermi")
+                print(final)
+            else:
+                final.append("Pico")
+                print(final)
+    if final == []:
+        final.append("Bagels")
+        print(final)
 
-                
-                
+    for x in final:
+        print(x)
+    print(final)
+    guess_count +=1
